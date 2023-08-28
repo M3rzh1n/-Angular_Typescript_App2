974361facd0971c899900aa49ae09c9e466e681d
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2023-08-28 12:06:08 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   auth0: {
     domain: 'dev-isoft.us.auth0.com',
     clientId: 'lERzBtf7cxB37irvf5ON8ZPjyXXnYYyR',
+    audience: 'https://ghost-api/',
     authorizationParams: {
       redirect_uri: 'https://m3rzh1n.github.io/Angular_Typescript_App2/callback',
     },
